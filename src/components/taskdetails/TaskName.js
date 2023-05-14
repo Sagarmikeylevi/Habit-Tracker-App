2a0999425aa44880d265d47412d3d233f3cdb1b1
unknown <sagarmikeylevi@gmail.com>
2023-05-14 20:46:23 +0530

    completed the styles of taskBar

--- src/components/taskdetails/TaskName.js
+++ src/components/taskdetails/TaskName.js
@@ -1,23 +1,38 @@
-import classes from './TaskName.module.css';
-import { FaCheck } from 'react-icons/fa';
+import classes from "./TaskName.module.css";
+import { FaCheck } from "react-icons/fa";
 
 const TaskName = () => {
-    return (
-      <div className={classes.wrapper}>
-        <div className={classes.taskBar}>
-          <div className={classes.image}>
+  return (
+    <div className={classes.wrapper}>
+      <ul className={classes.taskBarList}>
+        <li className={`${classes.taskBar} ${classes.checkedTaskBar}`}>
+          <div className={`${classes.taskIcon} ${classes.checkedTaskIcon}`}>
             <img
               src="https://cdn-icons-png.flaticon.com/128/2923/2923239.png"
               alt="glass of water"
             />
           </div>
-          <p>Drink Water</p>
-          <div className={classes.checkBox}>
-            <FaCheck className={classes.check} />
+          <p className={classes.checkedText}>Drink Water</p>
+          <div className={classes.checkedBox}>
+            <FaCheck className={classes.checked} />
           </div>
-        </div>
-      </div>
-    );
-}
+        </li>
+
+        <li className={`${classes.taskBar} ${classes.uncheckedTaskBar}`}>
+          <div className={`${classes.taskIcon} ${classes.uncheckedTaskIcon}`}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2923/2923239.png"
+              alt="glass of water"
+            />
+          </div>
+          <p className={classes.uncheckedText}>Drink Water</p>
+          <div className={classes.unCheckedBox}>
+            <FaCheck className={classes.unchecked} />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default TaskName;
