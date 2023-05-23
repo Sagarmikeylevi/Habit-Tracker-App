bfce5a4a84ae45cd9581936a97cd48f15f58b40d
unknown <sagarmikeylevi@gmail.com>
2023-05-23 21:41:36 +0530

    handled the task checkBox

--- src/components/tasks/TaskName.js
+++ src/components/tasks/TaskName.js
@@ -1,28 +1,59 @@
 import { useSelector } from "react-redux";
 import classes from "./TaskName.module.css";
 import { FaCheck, FaTrash } from "react-icons/fa";
+import { useState } from "react";
 
 const TaskName = () => {
   const tasks = useSelector((state) => state.habits.tasks);
 
+  const [taskCompletionStates, setTaskCompletionStates] = useState(
+    tasks.map(() => false)
+  );
+
+  const toggleTaskCompletion = (index) => {
+    setTaskCompletionStates((prevState) => {
+      const updatedStates = [...prevState];
+      updatedStates[index] = !updatedStates[index];
+      return updatedStates;
+    });
+  };
+
   return (
     <div className={classes.wrapper}>
       <ul className={classes.taskBarList}>
-        {tasks.map((task) => {
+        {tasks.map((task, index) => {
+          const isTaskCompleted = taskCompletionStates[index];
+          const taskBarCondition = isTaskCompleted
+            ? classes.checkedTaskBar
+            : classes.uncheckedTaskBar;
+          const taskIconCondition = isTaskCompleted
+            ? classes.checkedTaskIcon
+            : classes.uncheckedTaskIcon;
+          const textCondition = isTaskCompleted
+            ? classes.checkedText
+            : classes.uncheckedText;
+          const checkBoxCondition = isTaskCompleted
+            ? classes.checkedBox
+            : classes.unCheckedBox;
+          const isChecked = isTaskCompleted
+            ? classes.checked
+            : classes.unchecked;
+
           return (
             <li
-              className={`${classes.taskBar} ${classes.uncheckedTaskBar}`}
+              className={`${classes.taskBar} ${taskBarCondition}`}
               key={task.id}
             >
-              <div
-                className={`${classes.taskIcon} ${classes.uncheckedTaskIcon}`}
-              >
+              <div className={`${classes.taskIcon} ${taskIconCondition}`}>
                 <img src={task.categoryURL} alt="category icon" />
               </div>
-              <p className={classes.uncheckedText}>{task.title}</p>
+              <p className={textCondition}>{task.title}</p>
               <FaTrash className={classes.delete} />
-              <div className={classes.unCheckedBox}>
-                <FaCheck className={classes.unchecked} />
+              <div
+                className={checkBoxCondition}
+                onClick={() => toggleTaskCompletion(index)}
+              >
+                <FaCheck className={isChecked} />
               </div>
             </li>
           );
